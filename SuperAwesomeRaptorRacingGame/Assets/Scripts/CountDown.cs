e451cf7983b04f5d474e2bc680a3a434cb6faa62
Robin Staelens <robin.staelens@student.howest.be>
2018-12-03 14:10:23 +0100

    Integrated countdown in game

---
+++ SuperAwesomeRaptorRacingGame/Assets/Scripts/CountDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CountDown : MonoBehaviour
+{
+    public GameObject CountDownObject;
+    public AudioSource GetReady;
+    public AudioSource GoAudio;
+    public GameObject LapTimer;
+    public GameObject CarControl;
+
+
+	void Start ()
+	{
+	    StartCoroutine(CountStart());
+	}
+
+    IEnumerator CountStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        CountDownObject.GetComponent<Text>().text = "3";
+        GetReady.Play();
+        CountDownObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        CountDownObject.SetActive(false);
+        CountDownObject.GetComponent<Text>().text = "2";
+        GetReady.Play();
+        CountDownObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        CountDownObject.SetActive(false);
+        CountDownObject.GetComponent<Text>().text = "1";
+        GetReady.Play();
+        CountDownObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        CountDownObject.SetActive(false);
+        GoAudio.Play();
+        LapTimer.SetActive(true);
+        CarControl.SetActive(true);
+    }
+}
