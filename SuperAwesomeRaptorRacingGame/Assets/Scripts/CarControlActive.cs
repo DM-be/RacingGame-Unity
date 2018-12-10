9757d89c08369179b502c09dacc1f19aacec61df
Robin Staelens <robin.staelens@student.howest.be>
2018-12-10 11:44:31 +0100

    Added very basic AI with waypoints

--- SuperAwesomeRaptorRacingGame/Assets/Scripts/CarControlActive.cs
+++ SuperAwesomeRaptorRacingGame/Assets/Scripts/CarControlActive.cs
@@ -6,8 +6,10 @@ using UnityStandardAssets.Vehicles.Car;
 public class CarControlActive : MonoBehaviour
 {
     public GameObject CarControl;
+    public GameObject DreamCar01;
 	void Start ()
 	{
 	    CarControl.GetComponent<CarController>().enabled = true;
+	    DreamCar01.GetComponent<CarAIControl>().enabled = true;
 	}
 }
