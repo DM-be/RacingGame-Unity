30662b1b0d0f6751dc979bc9cd53fd0101db99f2
Robin Staelens <robin.staelens@student.howest.be>
2019-01-04 00:02:57 +0100

    Added camera objects for new cameras

---
+++ SuperAwesomeRaptorRacingGame/Assets/Scripts/CameraChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+    public GameObject NormalCam;
+    public GameObject FarCam;
+    public GameObject FPCam;
+    public int CamMode;
+
+	void Update () {
+		
+	}
+}
