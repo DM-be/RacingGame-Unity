36b0f94a7de3e7b4030e407bc3bbaf461d7be7e1
Robin Staelens <robin.staelens@student.howest.be>
2019-01-04 23:53:08 +0100

    Changed menus to add multiple cars

---
+++ SuperAwesomeRaptorRacingGame/Assets/Scripts/GlobalCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCar : MonoBehaviour
+{
+    //TODO enum
+    public static int CarType; // 0 = Red, 1 = Blue
+    public GameObject TrackWindow;
+
+    public void RedCar()
+    {
+        CarType = 0;
+        TrackWindow.SetActive(true);
+    }
+
+    public void BlueCar()
+    {
+        CarType = 1;
+        TrackWindow.SetActive(true);
+    }
+}
