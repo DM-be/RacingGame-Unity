80bdb303d3ccb62caee698e104ae8588700978fd
MorentDennis <dennis.morent@student.howest.be>
2019-01-04 12:29:48 +0100

    login script for loginscene

---
+++ SuperAwesomeRaptorRacingGame/Assets/Scripts/Login.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField userNameField;
+    public InputField passwordField;
+    public Button loginButton;
+
+    void Start()
+    {
+        loginButton.onClick.AddListener(submitLogin);
+    }
+
+
+    
+    public void submitLogin()
+    {
+        string userName = userNameField.text;
+        string password = passwordField.text;
+        
+       Debug.Log(userName);
+        Debug.Log(password);
+
+
+    }
+
+}
